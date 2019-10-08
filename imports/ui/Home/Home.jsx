35a9c8435f20a9df0ada391c3f29f6be9b69e615
Roger Winright <rogerwinright@gmail.com>
2019-10-08 17:33:25 -0400

    updated home styles

--- imports/ui/Home/Home.jsx
+++ imports/ui/Home/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return(
     <div className="Home">
-      {/* <h1>Home Component</h1> */}
+      <div className="content-container">
+        <h1>Home Component</h1>
+      </div>
     </div>
   )
 }
