a38dd6922b248f7935aa700e692c0354ff23464e
Roger Winright <rogerwinright@gmail.com>
2019-10-08 16:46:02 -0400

    set up SASS with project, begin work on NAV

--- imports/ui/Components/Nav/Nav.jsx
+++ imports/ui/Components/Nav/Nav.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className="Nav">
-
+      <ul>
+        <li>H</li>
+        <li>W</li>
+        <li>P</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
     </div>
   )
 }
