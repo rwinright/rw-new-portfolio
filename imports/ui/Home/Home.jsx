be9dd11159eb8a6766e1bdb0361374c371c584fd
Roger Winright <rogerwinright@gmail.com>
2019-10-10 10:01:43 -0400

    added typed styles

--- imports/ui/Home/Home.jsx
+++ imports/ui/Home/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Typed from 'typed.js';
 
 const Home = (props) => {
+  
+  let typed
+
+  useEffect(() => {
+    
+    const options = {
+      strings: ['<h1>Hello, my name is Roger Winright</h1>', '<h1>I like to build cool stuff on the Internet.</h1>'],
+      smartBackspace: true,
+      typeSpeed: 40
+    };
+
+    typed = new Typed('.typed', options)
+  }, []);
+
+  
 
   return(
     <div className="Home" ref={props.homeRef}>
       <img src='https://rw-portfolio.s3.us-east-2.amazonaws.com/code.jpg' alt="code background" />
       <div className="content-container">
-        <h1>Home Component</h1>
+        <span className="typed"/>
       </div>
     </div>
   )
