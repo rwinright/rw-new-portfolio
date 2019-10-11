279b5e97f4079e9bddb4887c63c63bcfbae934a2
Roger Winright <rogerwinright@gmail.com>
2019-10-10 23:12:30 -0400

    add base gallery styles.

--- imports/ui/Web/Web.jsx
+++ imports/ui/Web/Web.jsx
@@ -7,6 +7,20 @@ const Web = (props) => {
       <img src='https://rw-portfolio.s3.us-east-2.amazonaws.com/code.jpg' alt="code background" />
       <div className="content-container">
         <h1>Web Component</h1>
+        <div className="gallery">
+
+        </div>
+        <div className="gallery-controls">
+          <div className="control-arrow">
+            <span>{'<'}</span>
+          </div>
+          <div className="control-title">
+
+          </div>
+          <div className="control-arrow">
+            <span>{'>'}</span>
+          </div>
+        </div>
       </div>
     </div>
   )
